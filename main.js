f8a859cb18d1874744760f9cf060f961050c758c
Parish Neighbors <pn8bors@gmail.com>
2022-01-25 11:14:27 -0600

    Second commit

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 // ESM syntax is supported.
 const myStr = "hello class";
-const array1 = ['h', 'e', 'l', 'l', 'o', ' ', 'c', 'l', 'a', 's', 's'];
 
 console.log(myStr.replace('c', 'C'));
 console.log(myStr.replace('h', 'H'));
@@ -9,6 +8,6 @@ console.log(myStr.toUpperCase());
 console.log('hello class');
 console.log(myStr.repeat(5));
 console.log(myStr.replaceAll('hello class', 'Hello Class'));
-for (const element of array1) {
-    console.log(element);
+for (const value of myStr) {
+    console.log(value);
 }
